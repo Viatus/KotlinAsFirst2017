4336bb253953083077d98bf029399b18513bd17f
Viatus <roma-177@mail.ru>
2017-09-25 14:13:02 +0400

    some fixes

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson2.task2
 
 import lesson1.task1.sqr
@@ -17,9 +18,8 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean
-{
-    return (number / 1000 + number /100 % 10 == number % 10 + number % 100 / 10)
+fun isNumberHappy(number: Int): Boolean {
+    return (number / 1000 + number / 100 % 10 == number % 10 + number % 100 / 10)
 }
 
 /**
@@ -29,10 +29,10 @@ fun isNumberHappy(number: Int): Boolean
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun sqr (a : Int) : Int = a*a
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean
-{
-            return (y1 == y2 || x1 == x2 || sqr(x1 - x2) == sqr(y1 - y2))
+fun sqr(a: Int): Int = a * a
+
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return (y1 == y2 || x1 == x2 || sqr(x1 - x2) == sqr(y1 - y2))
 }
 
 /**
@@ -43,8 +43,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean
-{
+                 x2: Double, y2: Double, r2: Double): Boolean {
     return Math.sqrt(sqr(x1 - x2) + sqr(y1 - y2)) + r1 <= r2
 }
 
@@ -57,30 +56,23 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun min (a : Int, b : Int) : Int
-{
-    if (a < b)
-    {
+fun min(a: Int, b: Int): Int {
+    if (a < b) {
         return a
-    }
-    else
-    {
+    } else {
         return b
     }
 }
-fun max (a : Int, b : Int) : Int
-{
-    if (a > b)
-    {
+
+fun max(a: Int, b: Int): Int {
+    if (a > b) {
         return a
-    }
-    else
-    {
+    } else {
         return b
     }
 }
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean
-{
+
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
     val BrickLenght = min(min(a, b), c)
     val BrickHeihgt = max(max(a, b), c)
     val BrickWidth = a + b + c - BrickHeihgt - BrickLenght
--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -154,6 +154,7 @@ class Tests {
         assertEquals(0.0, sin(Math.PI, 1e-5), 1e-5)
         assertEquals(-1.0, sin(3.0 * Math.PI / 2.0, 1e-5), 1e-5)
         assertEquals(0.0, sin(100 * Math.PI, 1e-5), 1e-5)
+        assertEquals(0.10644440802791799, sin(-18.832102629018816, 0.3230520862758979), 1e-5)
     }
 
     @Test
